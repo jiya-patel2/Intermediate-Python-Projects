d0e1e07b96de79fb5f25222ef42835e8a00bf040
jiya-patel2 <jiya.advaita@gmail.com>
2026-03-05 22:43:30 +0530

    add scoreboard

--- snake_Game/main.py
+++ snake_Game/main.py
@@ -2,6 +2,8 @@ from turtle import Screen
 import time
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
+
 screen = Screen()
 screen.setup(width =600,height = 600)
 screen.title("Play with Python")
@@ -10,20 +12,26 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+score = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up,"Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.right, "Right")
 screen.onkey(snake.left,"Left")
+
 is_game_on = True
+
 while is_game_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
-    
+
     #detect collision with food 
     if snake.head.distance(food) < 13 :
-        food.refresh() 
-        
+        food.refresh()
+        score.new_score()
+
+    
+
 screen.exitonclick()
---
+++ snake_Game/scoreboard.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+ALIGNMENT = 'center'
+FONT = ('Courier', 20, 'normal')
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("#3B9797")
+        self.penup()
+        self.goto(0,270)
+        self.hideturtle()
+        self.score = 0
+        self.update_score()
+
+    def update_score(self):
+        self.write(f"[Score : {self.score}]", move=False, align=ALIGNMENT, font= FONT)
+
+    def new_score(self):
+        self.score += 1
+        self.clear()
+        self.update_score()
